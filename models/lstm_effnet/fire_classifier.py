b45ba73e2bb8d9741954f5fefa571ec8ab9943ca
NicolasIsla <nico.isla1@gmail.com>
2024-09-09 02:48:45 -0300

    effnet

---
+++ models/lstm_effnet/fire_classifier.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models import ResNet50_Weights
+from torchvision import transforms
+from PIL import Image
+import pytorch_lightning as pl
+
+from timm import create_model
+
+class FireClassifierv2(pl.LightningModule):
+    def __init__(self, lstm_layers=4):
+        super(FireClassifierv2, self).__init__()
+        self.save_hyperparameters()
+
+        # Usamos EfficientNetB3 como extractor de características.
+        efficientnet = create_model('efficientnet_b3', pretrained=True)
+
+        # Removemos la capa final de clasificación para usarla como extractor de características.
+        self.feature_extractor = nn.Sequential(*list(efficientnet.children())[:-2])
+
+        # Número de características de salida de EfficientNetB3.
+        num_features = efficientnet.classifier.in_features  # Cambiado según EfficientNetB3
+
+        # LSTM que procesará las características extraídas.
+        self.lstm = nn.LSTM(input_size=num_features * 4 * 4, hidden_size=256, batch_first=True, num_layers=lstm_layers)
+
+        # Capa de clasificación.
+        self.classifier = nn.Linear(256, 1)  # Salida binaria
+
+        # Dropout para regularización
+        self.dropout = nn.Dropout(0.2)
+
+
+    def forward(self, x):
+        # x shape: [batch_size, seq_length, channels, height, width]
+        # Procesa cada imagen de la secuencia a través del extractor de características.
+        batch_size, seq_length, C, H, W = x.size()
+        x = x.view(batch_size * seq_length, C, H, W)
+        x = self.feature_extractor(x)
+
+        # Reformatear salida para la LSTM
+        x = x.view(batch_size, seq_length, -1)
+
+        # Pasar las características por la LSTM
+        x, _ = self.lstm(x)
+
+        # Aplicamos la capa de clasificación a cada frame de la secuencia
+        # x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+
+    def preprocess_image(self, img):
+        img = self.resize(img)
+        img = self.to_tensor(img)
+        img = self.normalize(img)
+        return img
+
+    def infer_4_frames(self, image_paths):
+        images = [Image.open(img_path) for img_path in image_paths]
+        
+        preprocessed_images = [self.preprocess_image(img).unsqueeze(0) for img in images]
+        
+        input_tensor = torch.cat(preprocessed_images).unsqueeze(0)  # (1, 4, C, H, W)
+
+        self.eval()
+        with torch.no_grad():
+            output = self(input_tensor)
+            pred = torch.sigmoid(output).item()
+            return pred
--- models/model_loader.py
+++ models/model_loader.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import torch
 from models.lstm_resnet.fire_classifier import FireClassifier
+from models.lstm_effnet.fire_classifier import FireClassifierv2
 
 def load_model(model_type='yolov8', model_version='dummie_smoke.pt'):
     model_path = f'models/{model_type}/{model_version}.pt'
@@ -11,6 +12,8 @@ def load_model(model_type='yolov8', model_version='dummie_smoke.pt'):
         return torch.load(model_path)
     elif model_type == 'lstm_resnet':
         return FireClassifier.load_from_checkpoint(f'models/lstm_resnet/{model_version}.ckpt')
+    elif model_type == 'lstm_effnet':
+        return FireClassifierv2.load_from_checkpoint(f'models/lstm_effnet/{model_version}.ckpt')
     else:
         raise ValueError(f"Model {model_type} not supported")
 
